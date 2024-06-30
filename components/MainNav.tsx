6bcdfc98df49642059b43cdd9f80b558a3fb9ebd
Qedric <qedricjames@gmail.com>
2024-06-30 16:54:16 +0200

    fixed main nav - changed to client

---
+++ components/MainNav.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import Link from "next/link"
+
+export const MainNav = () => {
+
+    const ActiveLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+        //const router = useRouter()
+        const isActive = usePathname() === href
+      
+        return (
+          <Link href={href} className={isActive ? 'active' : ''}>{children}</Link>
+        )
+    }
+
+    return (
+        <nav className="w-fit flex items-center">
+            <ul className="flex gap-x-4 text-3xl">
+              <li>
+                <ActiveLink href="/gallery">Gallery</ActiveLink>
+              </li>
+              <li>
+                <ActiveLink href="/">My Cards</ActiveLink>
+              </li>
+              <li>
+                <ActiveLink href="/about">About</ActiveLink>
+              </li>
+              <li>
+                <ActiveLink href="/team">Team</ActiveLink>
+              </li>
+            </ul>
+          </nav>
+    )
+}
