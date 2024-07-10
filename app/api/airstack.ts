90d422b04b51c709817b5e983944bdebe6ecf20c
Qedric <qedricjames@gmail.com>
2024-07-10 16:19:21 +0200

    added airstack sdk

---
+++ app/api/airstack.ts
@@ -0,0 +1,41 @@
+/* const TrackFrameCasts = {
+    FarcasterCasts(
+      input: {blockchain: ALL, filter: {frameUrl: {_eq: "https://farconic.xyz/cn-tower"}}}
+    ) {
+      Cast {
+        castedAtTimestamp
+        text
+        castedBy {
+          profileName
+          socialCapital {
+            socialCapitalRank
+          }
+          location
+          followerCount
+        }
+        frame {
+          frameUrl
+        }
+        fid
+      }
+    }
+  } */
+/* 
+import { init, useQuery } from "@airstack/airstack-react"
+
+init("api-key"); */
+
+// example for component:
+/* const runQuery = (query:string, variables:any) => {
+  const { data, loading, error } = useQuery(query, variables, { cache: false });
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+
+  // Render your component using the data returned by the query
+}; */
